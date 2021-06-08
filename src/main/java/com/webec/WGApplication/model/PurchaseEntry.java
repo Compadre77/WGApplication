bb9bad240f07db00e4966c1cf9a66c8917282dd2
Daniel da Cruz <daniel.dacruz@students.fhnw.ch>
2021-06-08 17:13:19 +0200

    WIP: "wanted to add a new attribute to Purchase but its not quite working yet..."

--- src/main/java/com/webec/WGApplication/model/PurchaseEntry.java
+++ src/main/java/com/webec/WGApplication/model/PurchaseEntry.java
@@ -2,11 +2,13 @@ package com.webec.WGApplication.model;
 
 public class PurchaseEntry {
     public int id;
+    int amount;
     public String description;
     public boolean checked;
 
-    public PurchaseEntry(int id, String description, boolean checked){
+    public PurchaseEntry(int id, int amount, String description, boolean checked){
         this.id = id;
+        this.amount = amount;
         this.description = description;
         this.checked = checked;
     }
