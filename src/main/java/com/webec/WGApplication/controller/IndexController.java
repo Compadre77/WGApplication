63218a6f4dd2494994c38e1dd90aec98a7eb2f58
Daniel da Cruz <daniel.dacruz@students.fhnw.ch>
2021-06-15 10:35:31 +0200

    WIP: adapting IndexController

--- src/main/java/com/webec/WGApplication/controller/IndexController.java
+++ src/main/java/com/webec/WGApplication/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.webec.WGApplication.controller;
 
 import com.webec.WGApplication.model.entity.User;
 import com.webec.WGApplication.service.BillService;
+import com.webec.WGApplication.service.ToDoService;
 import com.webec.WGApplication.service.UserService;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -11,8 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
     private final BillService billservice;
+    private final ToDoService todoservice;
 
-    public IndexController(BillService billservice) {this.billservice = billservice; }
+    public IndexController(BillService billservice, ToDoService todoservice) {
+        this.billservice = billservice;
+        this.todoservice = todoservice;
+    }
 
     @RequestMapping("/")
     public String index(Model model) {
