84c96d285fc05e071fd2d6eb6a079ec3358b00da
Daniel da Cruz <daniel.dacruz@students.fhnw.ch>
2021-06-18 10:37:46 +0200

    Wrote test for PurchaseController.

---
+++ src/test/java/com/webec/WGApplication/entity/PurchaseEntityTest.java
@@ -0,0 +1,13 @@
+package com.webec.WGApplication.entity;
+
+import com.webec.WGApplication.model.entity.Purchase;
+import org.junit.Test;
+
+public class PurchaseEntityTest {
+
+    @Test
+    public void propertiesAreSetOnConstructor(){
+
+        Purchase testObject = new Purchase();
+    }
+}
---
+++ src/test/java/com/webec/WGApplication/entity/UserEntryTest.java
@@ -0,0 +1,25 @@
+//package com.webec.WGApplication.entity;
+//
+//import com.webec.WGApplication.model.UserEntry;
+//import org.junit.Assert;
+//import org.junit.Test;
+//import org.springframework.security.core.GrantedAuthority;
+//
+//import java.util.Collection;
+//
+//public class UserEntryTest {
+//    int id = 1;
+//    String username = "test";
+//    String password = "test";
+//    Collection<? extends GrantedAuthority> roles = null;
+//
+//    @Test
+//    public void propertiesAreSetOnConstructor(){
+//    UserEntry testObject = new UserEntry(id, username, password, roles);
+//
+//        Assert.assertEquals(id, testObject.id);
+//        Assert.assertEquals(username, testObject.username);
+//        Assert.assertEquals(password, testObject.password);
+//        Assert.assertEquals(roles, testObject.roles);
+//    }
+//}
